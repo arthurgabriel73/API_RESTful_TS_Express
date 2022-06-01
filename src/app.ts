5b1ce53bcbdea2de4a806344b6bd4f154c3945fe
Arthur Gabriel <arthurgabriel73@gmail.com>
2022-06-01 10:37:24 -0300

    Initiate express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Project created!!!")
+import express from "express"
+import config from "config"
+
+const app = express()
+
+//JSON middleware
+app.use(express.json());
+
+// app port
+const port = config.get<number>("port");
+
+app.listen(port, async () => {
+    console.log(`The application is running on the port: ${port}`);
+});
