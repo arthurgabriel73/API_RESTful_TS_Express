a747949b08b3118a80a3c66243acf4470ff804d4
Arthur Gabriel <arthurgabriel73@gmail.com>
2022-06-01 11:23:23 -0300

    Connects to database

---
+++ config/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "config";
+
+async function connect() {
+
+    const dbUri = config.get<string>("dbUri")
+
+    try {
+
+        await mongoose.connect(dbUri)
+        console.log("Connected to database!")
+    } catch (e) {
+        console.log("The connection gone wrong.")
+        console.log(`Error: ${e}`)
+    }
+}
+
+export default connect;
--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,4 @@
 export default {
     port: 3000,
+    dbUri: "mongodb+srv://arthurgabriel73:bJyRjS92KK3vifzV@cluster0.smnsjw1.mongodb.net/?retryWrites=true&w=majority"
 };
